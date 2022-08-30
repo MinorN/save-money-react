bbd7c4fbcaf92cae7d133073cd46b53b3b956f53
qijiajun <1481700559@qq.com>
2022-08-30 23:27:00 +0800

    清理文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import './App.css';
-import styled from 'styled-components';
-
-const Button = styled.button`
-  color:red;
-  border: none;
-  background-color: green;
-  &:hover{
-    background-color: pink;
-  }
-`;
 
 function App() {
   return (
     <div>
-      <Button>你好</Button>
+      Hello
     </div>
   );
 }
