67e67e265ae9be904fdb07dab91134927d2cbe50
qijiajun <1481700559@qq.com>
2022-09-01 19:10:35 +0800

    fix Icon click

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -13,15 +13,20 @@ const NavWrapper = styled.nav`
     > li {
       width: 33.3%;
       text-align: center;
-      display: flex;
-      padding: 4px 0;
-      flex-direction: column;
-      align-items: center;
 
-      .icon {
-        width: 24px;
-        height: 24px;
+      > a {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 4px 0;
+
+        .icon {
+          width: 24px;
+          height: 24px;
+        }
       }
+
+
     }
   }
 `;
@@ -31,16 +36,13 @@ const Nav = () => {
         <NavWrapper>
             <ul>
                 <li>
-                    <Icon name={'tag'}/>
-                    <Link to="/tags">标签页</Link>
+                    <Link to="/tags"><Icon name={'tag'}/>标签页</Link>
                 </li>
                 <li>
-                    <Icon name={'money'}/>
-                    <Link to="/money">记账页</Link>
+                    <Link to="/money"><Icon name={'money'}/>记账页</Link>
                 </li>
                 <li>
-                    <Icon name={'chart'}/>
-                    <Link to="/Statistics">统计页</Link>
+                    <Link to="/Statistics"><Icon name={'chart'}/>统计页</Link>
                 </li>
             </ul>
         </NavWrapper>
