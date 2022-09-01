49f2c6c6d8dab1709d0866f69ec3814e4f1995b0
qijiajun <1481700559@qq.com>
2022-09-01 18:15:03 +0800

    Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,37 @@
 import React from 'react';
 import {Route, HashRouter as Router, Switch, Redirect} from 'react-router-dom';
-import styled from 'styled-components';
-import Nav from './components/Nav';
-
-
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
-const Main = styled.div`
-  flex-grow: 1;
-  overflow: hidden;
-`;
-
+import Layout from './components/Layout';
 
 function App() {
     return (
         <Router>
-            <Wrapper>
-                <Main>
-                    <Switch>
-                        <Route path="/tags"><Tags/></Route>
-                        <Route path="/money"><Money/></Route>
-                        <Route path="/Statistics"><Statistics/></Route>
-                        <Redirect exact from="/" to="/money"/>
-                        <Route path="*"><NoMatch/></Route>
-                    </Switch>
-                </Main>
-                <Nav/>
-            </Wrapper>
+            <Switch>
+                <Route path="/tags"><Tags/></Route>
+                <Route path="/money"><Money/></Route>
+                <Route path="/Statistics"><Statistics/></Route>
+                <Redirect exact from="/" to="/money"/>
+                <Route path="*"><NoMatch/></Route>
+            </Switch>
         </Router>
     );
 }
 
 function Statistics() {
-    return <h2>Statistics</h2>;
+    return (
+        <Layout><h2>Statistics</h2></Layout>
+    );
 }
 
 function Tags() {
-    return <h2>Tags</h2>;
+    return (
+        <Layout><h2>tag</h2></Layout>
+    );
 }
 
 function Money() {
-    return <h2>Money</h2>;
+    return (
+        <Layout><h2>money</h2></Layout>
+    );
 }
 
 function NoMatch() {
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
-import Icon from './icon';
+import Icon from './Icon';
 import React from 'react';
 
 const NavWrapper = styled.nav`
