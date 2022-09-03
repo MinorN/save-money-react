2b6bc7529164d57bfe10b038c2c27fc29c6c4ede
qijiajun <1481700559@qq.com>
2022-09-03 17:37:18 +0800

    完成标签的选择和新增

--- src/views/Money/TgasSection.tsx
+++ src/views/Money/TgasSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const TagsSection = styled.section`
+const Wrapper = styled.section`
   background-color: #fff;
   padding: 12px 16px;
   flex-grow: 1;
@@ -19,6 +20,10 @@ const TagsSection = styled.section`
       padding: 3px 18px;
       font-size: 14px;
       margin: 8px 12px;
+
+      &.selected {
+        background-color: #f60;
+      }
     }
   }
 
@@ -32,4 +37,41 @@ const TagsSection = styled.section`
   }
 `;
 
-export default TagsSection
+const TagsSection: React.FC = () => {
+    const [tags, setTage] = useState<string[]>(['衣', '食', '住', '行']);
+    const [selectedTags, setSelectedTags] = useState<string[]>([]);
+    const onAddTag = () => {
+        const tagName = window.prompt('新标签名称为：');
+        if (tagName !== null) {
+            setTage([...tags, tagName]);
+        }
+    };
+    const onToggleTag = (tag: string) => {
+        const index = selectedTags.indexOf(tag);
+        if (index >= 0) {
+            // 如果tag被选中，复制所有没被选中的tag 作为新的selectedTags
+            setSelectedTags(selectedTags.filter(t => t !== tag));
+        } else {
+            setSelectedTags([...selectedTags, tag]);
+        }
+    };
+    const getClass = (tag: string) => {
+        return selectedTags.indexOf(tag) >= 0 ? 'selected' : '';
+    };
+    return (
+        <Wrapper>
+            <ol>
+                {tags.map(tag =>
+                    <li key={tag} onClick={() => {
+                        onToggleTag(tag);
+                    }} className={getClass(tag)}>
+                        {tag}
+                    </li>
+                )}
+            </ol>
+            <button onClick={onAddTag}>新增标签</button>
+        </Wrapper>
+    );
+};
+
+export default TagsSection;
